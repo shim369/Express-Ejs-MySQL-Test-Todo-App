22361c01b5111290f947026a54c24c074db9970c
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-03-29 19:14:31 +0900

    add async/await

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let router = express.Router();
-const mysql = require('mysql');
+const connection = require('mysql2-promise')();
 
-const connection = mysql.createConnection({
+connection.configure({
   host: 'localhost',
   user: 'root',
   password: '',
@@ -10,52 +10,44 @@ const connection = mysql.createConnection({
   authPlugin: 'none',
 });
 
-router.get('/', function (req, res, next) {
-  connection.query(
-    `select * from tasks;`,
-    (error, results) => {
-      console.log(error);
-      console.log(results);
-      res.render('index', {
-        title: 'ToDo List App',
-        todos: results,
-      });
-    }
-  );
+router.get('/', async (req, res) => {
+  try {
+    const [results] = await connection.query('select * from tasks;');
+    console.log(results);
+    res.render('index', {
+      title: 'ToDo List App',
+      todos: results,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error retrieving tasks');
+  }
 });
 
-router.post('/', function(req, res, next) {
-  connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting:', err);
-    } else {
-      console.log('Connected to MySQL server');
-    }
-  });
-  const todo = req.body.add;
-  connection.query(
-    `insert into tasks (user_id, content) values (1, '${todo}');`,
-    (error, results) => {
-      console.log(error);
-      res.redirect('/');
-    }
-  );
+router.post('/', async (req, res) => {
+  try {
+    const todo = req.body.add;
+    const [results] = await connection.query(
+      `insert into tasks (user_id, content) values (1, '${todo}');`
+    );
+    console.log(results);
+    console.log('Task added successfully');
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error adding task' });
+  }
 });
 
-router.post('/delete/:id', function(req, res, next) {
+router.post('/delete/:id', async (req, res) => {
   const id = req.params.id;
-  connection.query(
-    `delete from tasks where id = ?;`,
-    [id],
-    (error, results) => {
-      console.log(error);
-      if (error) {
-        res.status(500).send('Error deleting task');
-      } else {
-        res.redirect('/');
-      }
-    }
-  );
+  try {
+    await connection.query('delete from tasks where id = ?;', [id]);
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error deleting task');
+  }
 });
 
 module.exports = router;
